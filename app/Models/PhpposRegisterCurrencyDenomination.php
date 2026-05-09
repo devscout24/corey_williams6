835257ed5a15c0225ca6488a2dd14706e8c9ade9
Moskov-1 <wwwronyraihan123@gmail.com>
2026-05-10 04:37:57 +0600

    feat: add PhpposRegisterCurrencyDenomination model and migration for currency denominations table

---
+++ app/Models/PhpposRegisterCurrencyDenomination.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PhpposRegisterCurrencyDenomination extends Model
+{
+    protected $table = 'phppos_register_currency_denominations';
+    protected $guarded = [];
+}
---
+++ database/migrations/2026_05_09_223121_create_phppos_register_currency_denominations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phppos_register_currency_denominations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('value', 23, 10);
+            $table->integer('deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phppos_register_currency_denominations');
+    }
+};
